ea7f6b43a1a9838ad97a1f76e9be059e9195ef7d
TyDa3998 <33527364+TyDa3998@users.noreply.github.com>
2017-11-09 16:06:17 -0700

    Add files via upload

---
+++ Pseudo_Code/Pseudo_Code.ino
@@ -0,0 +1,47 @@
+void setup() {
+  i = 0;
+}
+
+void loop() {
+  while(i=0)  //Temp Loop
+  {
+    read/display temperature data
+
+      if(button is pushed)
+      {
+        i+=1;
+      }
+    delay(250);
+  }
+
+  while(i=1)  //Humid Loop
+  {
+    read/display Humidity Data    
+      if(button is pushed)
+      {
+        i+=1;
+      }
+    delay(250);
+  }
+
+  while(i=2)  //Wind Loop
+  {
+    read/display wind speed data    
+      if(button is pushed)
+      {
+        i+=1;
+      }
+    delay(250);
+  }
+
+  while(i=3)  //CO2 Loop
+  {
+    read/display CO2 data    
+      if(button is pushed)
+      {
+        i=0;  //Reset to Temp Loop
+      }
+    delay(250);
+  }
+
+}
